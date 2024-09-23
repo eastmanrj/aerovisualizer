c7927d1d8214437ce140e123272f98111ac5ea89
R Eastman <eastmanrj@users.noreply.github.com>
2024-09-23 09:07:00 -0700

    v0.2.1, minor changes to main 2

--- packages/main/src/js/main.js
+++ packages/main/src/js/main.js
@@ -18,16 +18,16 @@ const infoReturnButton = document.getElementById('info-return-btn');
 const buttonElements = document.getElementById('btn-elements');
 const infoElements = document.getElementById('info-elements');
 
-buttonElements.style.display = 'grid';
+buttonElements.style.display = 'flex';
 infoElements.style.display = 'none';
 
 const toggleShowInfo = function(){
   if (buttonElements.style.display === 'none'){
-    buttonElements.style.display = 'grid';
+    buttonElements.style.display = 'flex';
     infoElements.style.display = 'none';
   }else{
     buttonElements.style.display = 'none';
-    infoElements.style.display = 'grid';
+    infoElements.style.display = 'flex';
   }
 }
 
